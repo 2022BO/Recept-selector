bc93d3bfd923b9760d7b520a918353f49d73d35c
Suzanne Boersma <suzanneboersma@outlook.com>
2023-10-21 13:58:26 +0200

    RecipeSearch

---
+++ src/RecipeSearch.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { data } from './utils/data';
+import { RecipeListPage } from './RecipeListPage';
+import { TextInput } from '@chakra-ui/react';
+
+export const RecipeSearch = ({ clickFn }) => {
+  const [searchField, setSearchField] = useState('');
+
+  const matchedRecipes = data.filter((recipe) => {
+    return recipe.recipe.label
+      .toLowerCase()
+      .includes(searchField.toLowerCase());
+  });
+
+  const handleChange = (event) => {
+    setSearchField(event.target.value);
+  };
+
+  return (
+    <>
+      <label>Search for recipes:</label>
+      <TextInput onChange={handleChange} w={200} mb={8} />
+      <RecipeListPage clickFn={clickFn} recipes={recipes} />
+    </>
+  );
+};
