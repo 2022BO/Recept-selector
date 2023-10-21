9ed23745ab0b013577fe6e761673278ae716484b
Suzanne Boersma <suzanneboersma@outlook.com>
2023-10-21 14:34:27 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,49 @@
+import { useState } from 'react';
+
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 import { RecipeListPage } from './pages/RecipeListPage';
+import RecipePage from './pages/RecipePage';
 
 export const App = () => {
-  // Your state code here
-  return <RecipeListPage />;
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  return (
+    <div>
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="Options"
+          icon={<HamburgerIcon />}
+          variant="outline"
+          size="md"
+          colorScheme="green"
+          _hover={{ bgColor: 'green.500' }}
+        >
+          Open menu
+        </MenuButton>
+        <MenuList>
+          <MenuItem as="a" href="/recipe-list">
+            Bekijk onze Recepten
+          </MenuItem>
+          <MenuItem as="a" href="/recipe/123">
+            Bekijk Specifiek Recept
+          </MenuItem>
+        </MenuList>
+      </Menu>
+
+      {selectedRecipe ? (
+        <RecipePage recipe={selectedRecipe} goBack={setSelectedRecipe} />
+      ) : (
+        <RecipeListPage setSelectedRecipe={setSelectedRecipe} />
+      )}
+    </div>
+  );
 };
+export default App;
