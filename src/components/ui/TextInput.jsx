d1528996be6f10e83ede7dd708d1051ac022b604
Suzanne Boersma <suzanneboersma@outlook.com>
2023-10-22 08:35:24 +0200

    TextInput.jsx

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -9,24 +9,24 @@ import {
 } from '@chakra-ui/react';
 import { Tag } from './RecipeItemPage';
 
-export const TextInput = ({ item, clickFn }) => {
+export const TextInput = ({ recipe, clickFn }) => {
   return (
     <Card
       borderRadius="xl"
       w="sm"
       h="30rem"
-      onClick={() => clickFn(item)}
+      onClick={() => clickFn(recipe)}
       cursor="pointer"
       _hover={{ transform: 'scale(1.01)' }}
     >
       <CardBody>
-        <Image h={64} w="sm" src={item.imageUrl} borderRadius="xl" />
+        <Image h={64} w="sm" src={recipe.image} borderRadius="xl" />
         <Stack mt="6" spacing="3">
-          <Heading size="md">{item.title}</Heading>
-          <Text>{item.summary}</Text>
+          <Heading size="md">{recipe.label}</Heading>
+          <Text>{recipe.label}</Text>
           <Flex gap={2}>
-            {item.skills.map((skill) => (
-              <Tag key={skill}>{skill}</Tag>
+            {recipe.healthLabels.map((recipe) => (
+              <Tag key={label}>{label}</Tag>
             ))}
           </Flex>
         </Stack>
