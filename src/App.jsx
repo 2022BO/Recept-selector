f58f8ee7c2612fa3659c606b7b9c4c11966840d3
Suzanne Boersma <suzanneboersma@outlook.com>
2023-10-22 08:33:11 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,17 @@
 import { useState } from 'react';
-
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  IconButton,
-} from '@chakra-ui/react';
-import { HamburgerIcon } from '@chakra-ui/icons';
 import { RecipeListPage } from './pages/RecipeListPage';
-import {RecipePage} from './pages/RecipePage';
+import { RecipePage } from './pages/RecipePage';
 
 export const App = () => {
-  const [selectedRecipe, setSelectedRecipe] = useState(null);
-
-  const greeting = "Welcome to our restaurant ";
+  const [selectedItem, setSelectedItem] = useState();
 
   return (
     <div>
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Options"
-          icon={<HamburgerIcon />}
-          variant="outline"
-          size="md"
-          colorScheme="green"
-          _hover={{ bgColor: 'green.500' }}
-        >
-          Open menu
-        </MenuButton>
-        <MenuList>
-          <MenuItem as="a" href="/recipe-list">
-            Bekijk onze Recepten
-          </MenuItem>
-          <MenuItem as="a" href="/recipe/123">
-            Bekijk Specifiek Recept
-          </MenuItem>
-        </MenuList>
-      </Menu>
-
-      {selectedRecipe ? (
-        <RecipePage recipe={selectedRecipe} onClick={setSelectedRecipe} />
+      {selectedItem ? (
+        <RecipeListPage recipe={selectedItem} clickFn={setSelectedItem} />
       ) : (
-        <>
-        <h1>{greeting}</h1>
-        <RecipeListPage onClick={setSelectedRecipe} />
-        </>
+        <RecipePage clickFn={setSelectedItem} />
       )}
     </div>
-   
   );
 };
-
