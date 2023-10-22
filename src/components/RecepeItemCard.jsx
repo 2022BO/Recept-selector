c60b3ad4391c2c60b4e90c6e6b3f9c7df60a4697
Suzanne Boersma <suzanneboersma@outlook.com>
2023-10-22 08:55:46 +0200

    RecepeItemCard.jsx

---
+++ src/components/RecepeItemCard.jsx
@@ -0,0 +1,73 @@
+import {
+  Card,
+  Image,
+  CardBody,
+  Stack,
+  Heading,
+  Text,
+  Flex,
+} from '@chakra-ui/react';
+import { Tag } from './ui/RecipeItemPage';
+
+export const TextInput = ({ recipe, clickFn }) => {
+  return (
+    <Card
+      borderRadius="xl"
+      w="sm"
+      h="30rem"
+      onClick={() => clickFn(recipe)}
+      cursor="pointer"
+      _hover={{ transform: 'scale(1.01)' }}
+    >
+      <CardBody>
+        <Image h={64} w="sm" src={recipe.image} borderRadius="xl" />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">{recipe.label}</Heading>
+          <Text>{recipe.label}</Text>
+          <Flex gap={2}>
+            {recipe.healthLabels.map((recipe) => (
+              <Tag key={recipe}>{recipe}</Tag>
+            ))}
+          </Flex>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+};
+
+/*import {
+  Card,
+  Image,
+  CardBody,
+  Stack,
+  Heading,
+  Text,
+  Flex,
+} from '@chakra-ui/react';
+
+
+export const TextInput= ({ onClick, recipe }) => {
+  return (
+    <Card
+      borderRadius="xl"
+      w="sm"
+      h="30rem"
+      onClick={() => onClick(recipe)}
+      cursor="pointer"
+      _hover={{ transform: 'scale(1.01)' }}
+    >
+      <CardBody>
+        <Image h={64} w="sm" src={item.imageUrl} borderRadius="xl" />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">{recipe.label}</Heading>
+          <Text>{recipe.mealType}</Text>
+          <Flex gap={2}>
+            {hits.recipe.map((recipe) => (
+              <Tag key={recipe}>{dishType}</Tag>
+            ))}
+          </Flex>
+        </Stack>
+      </CardBody>
+    </Card>
+  );
+};*/
